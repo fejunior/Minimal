a15d9bfd41c29b195b256542d07101ebe1ef9efc
fejun <fejunior5@hotmail.com>
2021-12-29 06:49:52 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Minimal.Data
+{
+    public class AppDbContext : DbContext
+    {//representação do banco de dados em memoria
+        //Representação da tabela
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=app.db;Cache=shared");
+    }
+}
